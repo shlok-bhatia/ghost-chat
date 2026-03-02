6cee45fa3d4f81d220e7e95b521ef6455a6e0650
shlok-bhatia <shlokbhatia16@gmail.com>
2026-03-02 14:08:37 +0530

    Change socket connection URL to production server

--- app/socket.ts
+++ app/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://192.168.31.212:3000", {
+export const socket = io("https://ghost-chat-uenp.onrender.com", {
     transports: ["websocket"],
      autoConnect: true,
 });
