9ba896f187fd5efd18d835e66585f0f8632107a7
shlok <shlokbhatia16@gmail.com>
2026-03-23 19:23:42 +0530

    server hanges

--- app/socket.ts
+++ app/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://192.168.1.109:3000", {
+export const socket = io(" https://ghost-chat-uenp.onrender.com", {
     transports: ["websocket"],
      autoConnect: true,   
 }
